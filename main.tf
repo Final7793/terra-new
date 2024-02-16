72afe434e8d828f0e4f3da773add3401ec5e5711
ANPATIL\arunp <arun>
2024-02-16 20:21:13 +0530

    added

--- main.tf
+++ main.tf
@@ -2,8 +2,29 @@ provider "aws" {
     region = var.aws_region
 }
 
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]  # Canonical
+}
+
+
 resource "aws_instance" "my-instance" {
-    ami = "ami-0449c34f967dbf18a"
+    ami = data.aws_ami.ubuntu.id
+
+
+
     instance_type = var.instance_type
 }
 
