afc0406b4625d0f6355642c078fb930b1c89adc4
ANPATIL\arunp <arun>
2024-02-16 23:49:00 +0530

    added

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_security_group" "my-sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.my-sg.id
-  cidr_ipv4         = aws_vpc.main.cidr_block
+  
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
@@ -49,7 +49,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
   security_group_id = aws_security_group.my-sg.id
-  cidr_ipv6         = aws_vpc.main.ipv6_cidr_block
+  
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
