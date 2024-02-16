283e2b40da37e32541fbab3af27fa4dbf916869a
ANPATIL\arunp <arun>
2024-02-16 23:51:15 +0530

    added

--- main.tf
+++ main.tf
@@ -29,40 +29,26 @@ resource "aws_instance" "my-instance" {
     instance_type = var.instance_type
 }
 
-resource "aws_security_group" "my-sg" {
-  name        = "my-sg"
-  description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = "vpc-0234e221805288aa7"
 
-  tags = {
-    Name = "allow_tls"
+
+resource "aws_security_group" "example" {
+  name        = "example-security-group"
+  description = "Example security group created with Terraform"
+    vpc_id      = "vpc-0234e221805288aa7"
+
+  // Ingress rules
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-}
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.my-sg.id
-  
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
-  security_group_id = aws_security_group.my-sg.id
-  
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
-}
-
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.my-sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
-
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-  security_group_id = aws_security_group.my-sg.id
-  cidr_ipv6         = "::/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
+  // Egress rules
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
