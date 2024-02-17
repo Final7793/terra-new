6d3a7e97ad7d3c4a81ee495edd8b906252ca0b7e
ANPATIL\arunp <arun>
2024-02-17 12:50:38 +0530

    Added

--- terraform-temp/main.tf
+++ terraform-temp/main.tf
@@ -3,9 +3,13 @@ provider "aws" {
 }
 
 
-resource "aws_instance" my-instance" {
-    ami = "ami-0449c34f967dbf18a"
-    instance_type = "t2.micro"
-    }
 
-   
+   resource "aws_instance" "example" {
+  ami           = "ami-0449c34f967dbf18a"  # Specify your desired AMI ID
+  instance_type = "t2.micro"      # Specify your desired instance type
+ 
+
+  tags = {
+    Name = "example-instance"  # Specify your desired name for the instance
+  }
+}
